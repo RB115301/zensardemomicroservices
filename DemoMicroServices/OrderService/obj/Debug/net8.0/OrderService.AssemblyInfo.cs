70d1dd363c18633ae6db60bfc0fb425ee5cfc70d
RB115301 <RB115301@GGOW42NLPC1986>
2025-05-06 14:59:29 +0530

    Minor changes

--- DemoMicroServices/OrderService/obj/Debug/net8.0/OrderService.AssemblyInfo.cs
+++ DemoMicroServices/OrderService/obj/Debug/net8.0/OrderService.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("OrderService")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d2f5b1f3963ed22d25ecc81e6b49005927631a25")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cd36cedc20c6f206ae4e6f371fb911e5f5c6b144")]
 [assembly: System.Reflection.AssemblyProductAttribute("OrderService")]
 [assembly: System.Reflection.AssemblyTitleAttribute("OrderService")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
