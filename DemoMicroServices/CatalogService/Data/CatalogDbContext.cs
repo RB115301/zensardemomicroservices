d2f5b1f3963ed22d25ecc81e6b49005927631a25
RB115301 <RB115301@GGOW42NLPC1986>
2025-05-06 11:49:20 +0530

    Created & tested CatalogService Project

---
+++ DemoMicroServices/CatalogService/Data/CatalogDbContext.cs
@@ -0,0 +1,17 @@
+﻿using CatalogService.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace CatalogService.Data
+{
+    public class CatalogDbContext : DbContext
+    {
+
+        public CatalogDbContext(DbContextOptions<CatalogDbContext> options) : base(options) 
+        { 
+        
+        }
+     
+        public DbSet<Product> Products { get; set; }
+
+    }
+}
