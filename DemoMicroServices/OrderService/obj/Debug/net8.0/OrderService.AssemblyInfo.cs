5897a9d1502aa119d46d7be54728933647dcb2e6
RB115301 <RB115301@GGOW42NLPC1986>
2025-05-06 15:12:54 +0530

    added docker file to order service

--- DemoMicroServices/OrderService/obj/Debug/net8.0/OrderService.AssemblyInfo.cs
+++ DemoMicroServices/OrderService/obj/Debug/net8.0/OrderService.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("OrderService")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cd36cedc20c6f206ae4e6f371fb911e5f5c6b144")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a33819be6500ed4167a2c8d0ba9d35a694f5f8c0")]
 [assembly: System.Reflection.AssemblyProductAttribute("OrderService")]
 [assembly: System.Reflection.AssemblyTitleAttribute("OrderService")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
